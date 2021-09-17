1d1e70080ee81d489561ac697194dca032b35e98
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-09-17 10:03:12 +0200

    ion-datetime - dummy example part 1

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -43,6 +43,11 @@ export class InicioPage implements OnInit {
       icon: 'checkmark-circle-outline',
       name: 'Checkbox',
       redirectTo: '/check'
+    },
+    {
+      icon: 'calendar-outline',
+      name: 'DateTime',
+      redirectTo: '/date-time'
     }
   ];
 
