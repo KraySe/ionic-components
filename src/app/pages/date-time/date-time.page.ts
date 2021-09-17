0380b8d0d1f14ab693e8cf794e1d8540de00a8a1
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-09-17 10:12:48 +0200

    ion-datetime - dummy example part 2

--- src/app/pages/date-time/date-time.page.ts
+++ src/app/pages/date-time/date-time.page.ts
@@ -9,6 +9,26 @@ export class DateTimePage implements OnInit {
 
   fechaNaci: Date = new Date();
 
+  customYearValues = [2025, 2020, 2016, 2008, 2004, 2000, 1996];
+
+  customPickerOptions = {
+    buttons: [
+      {
+        text: 'Hola',
+        handler: (event) => {
+          console.log(event);
+        }
+      },
+      {
+        text: 'Mundo',
+        handler: () => {
+          console.log('log!');
+        }
+      }
+    ]
+  };
+
+
   constructor() { }
 
   ngOnInit() {
