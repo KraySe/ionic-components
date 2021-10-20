5157fe0ad6ad4261917cfe565a023430351c9f9b
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-10-20 10:37:46 +0200

    input forms basic example - 02

--- src/app/pages/input/input.page.ts
+++ src/app/pages/input/input.page.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.page.html',
   styleUrls: ['./input.page.scss'],
 })
+
 export class InputPage implements OnInit {
 
   nombre: string = 'Paco';
+  usuario: any = {
+    email: '',
+    password: ''
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  public onSubmit(formulario: NgForm) {
+    console.log('submit');
+    console.log(this.usuario);
+    console.log(formulario);
   }
 
 }
