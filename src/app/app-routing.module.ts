d6cc9408246763c0babd9f081f1b713217b3051d
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-10-21 17:23:55 +0200

    ion modal dummy example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,40 +33,44 @@ const routes: Routes = [
   },
   {
     path: 'check',
-    loadChildren: () => import('./pages/check/check.module').then( m => m.CheckPageModule)
+    loadChildren: () => import('./pages/check/check.module').then(m => m.CheckPageModule)
   },
   {
     path: 'date-time',
-    loadChildren: () => import('./pages/date-time/date-time.module').then( m => m.DateTimePageModule)
+    loadChildren: () => import('./pages/date-time/date-time.module').then(m => m.DateTimePageModule)
   },
   {
     path: 'fab',
-    loadChildren: () => import('./pages/fab/fab.module').then( m => m.FabPageModule)
+    loadChildren: () => import('./pages/fab/fab.module').then(m => m.FabPageModule)
   },
   {
     path: 'grid',
-    loadChildren: () => import('./pages/grid/grid.module').then( m => m.GridPageModule)
+    loadChildren: () => import('./pages/grid/grid.module').then(m => m.GridPageModule)
   },
   {
     path: 'infinite',
-    loadChildren: () => import('./pages/infinite/infinite.module').then( m => m.InfinitePageModule)
+    loadChildren: () => import('./pages/infinite/infinite.module').then(m => m.InfinitePageModule)
   },
   {
     path: 'input',
-    loadChildren: () => import('./pages/input/input.module').then( m => m.InputPageModule)
+    loadChildren: () => import('./pages/input/input.module').then(m => m.InputPageModule)
   },
   {
     path: 'list',
-    loadChildren: () => import('./pages/list/list.module').then( m => m.ListPageModule)
+    loadChildren: () => import('./pages/list/list.module').then(m => m.ListPageModule)
   },
   {
     path: 'list-reorder',
-    loadChildren: () => import('./pages/list-reorder/list-reorder.module').then( m => m.ListReorderPageModule)
+    loadChildren: () => import('./pages/list-reorder/list-reorder.module').then(m => m.ListReorderPageModule)
   },
   {
     path: 'loading',
-    loadChildren: () => import('./pages/loading/loading.module').then( m => m.LoadingPageModule)
+    loadChildren: () => import('./pages/loading/loading.module').then(m => m.LoadingPageModule)
   },
+  {
+    path: 'modal',
+    loadChildren: () => import('./pages/modal/modal.module').then(m => m.ModalPageModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/pages/modal-info/modal-info.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal-info',
+  templateUrl: './modal-info.page.html',
+  styleUrls: ['./modal-info.page.scss'],
+})
+export class ModalInfoPage implements OnInit {
+
+  constructor(
+    private modalCtrl: ModalController
+  ) { }
+
+  ngOnInit() {
+  }
+
+
+  public exitWithoutArguments() {
+    this.modalCtrl.dismiss();
+  }
+
+}
