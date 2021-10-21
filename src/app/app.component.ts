77d057f3e7e156e8becc13116006a284b8471d6b
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-10-21 16:20:53 +0200

    ion-menu dummy example part 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonItem } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/interfaces';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  menuOpt: Observable<Componente[]>;
+
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  ngOnInit() {
+    this.menuOpt = this.dataService.getMenuOpt();
+  }
 }
