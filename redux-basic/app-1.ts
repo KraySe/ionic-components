d8f93d9ce2b3291c6d5c67ceacdd40e93f4ed1b6
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-10-26 17:04:18 +0200

    More actions and basic reducer

--- redux-basic/app-1.ts
+++ redux-basic/app-1.ts
@@ -11,11 +11,21 @@ interface Action {
 
 
 function reducer(state = 10, action: Action) {
-    if (action.type === 'INCREMENTAR') {
-        return state += 1;
+    // if (action.type === 'INCREMENTAR') {
+    //     return state += 1;
+    // }
+    switch (action.type) {
+        case 'INCREMENT':
+            return state += 1;
+        case 'DECREMENT':
+            return state -= 1;
+        case 'MULTIPLY':
+            return state * action.payload;
+        case 'DIVIDE':
+            return state / action.payload;
+        default:
+            return state
     }
-
-    return state;
 }
 
 
@@ -24,4 +34,25 @@ const incrementAction: Action = {
     type: 'INCREMENT'
 };
 
-console.log(reducer(10, incrementAction));
+console.log(reducer(10, incrementAction));  // 11
+
+
+const decrementAction: Action = {
+    type: 'DECREMENT'
+};
+
+console.log(reducer(10, decrementAction));  // 9
+
+const multiplyAction: Action = {
+    type: 'MULTIPLY',
+    payload: 2
+};
+
+console.log(reducer(10, multiplyAction));  // 20
+
+const divideAction: Action = {
+    type: 'DIVIDE',
+    payload: 2
+};
+
+console.log(reducer(10, divideAction));  // 5
