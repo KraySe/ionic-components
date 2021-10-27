e41a1fbe33368fdf7566e2d6a436996118319ebd
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-10-27 09:58:04 +0200

    Class Store dummy

---
+++ redux-basic/app-3.ts
@@ -0,0 +1,29 @@
+import { Action, Reducer } from './ngrx-fake/ngrx.ts';
+import { contadorReducer } from './contador/contador.reducer.ts';
+import { incrementAction, multiplyAction } from './contador/contador.actions.ts';
+
+class Store<T> {
+    constructor(
+        private reducer: Reducer<T>,
+        private state: T
+    ) { }
+
+    public getState() {
+        return this.state;
+    }
+
+    public dispatch(action: Action) {
+        this.state = this.reducer(this.state, action);
+    }
+}
+
+
+const store = new Store(contadorReducer, 10)
+console.log(store.getState());
+
+store.dispatch(incrementAction);
+store.dispatch(incrementAction);
+console.log(store.getState()); // 12
+
+store.dispatch(multiplyAction);
+console.log(store.getState()); // 24
